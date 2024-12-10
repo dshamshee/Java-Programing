5c866767650566227f09e7f523356f0feff29e45
Danish <danishshamshee123@gmail.com>
2024-12-10 23:19:01 +0530

    some adding

--- ADBMS/CRUD.sql
+++ ADBMS/CRUD.sql
@@ -11,6 +11,20 @@ create table student (
 insert into student values ('24PG11000', 'Guddu Pandit', 'MCA', '1234567890', '312547856325', 'student@gmail.com', 'Vadodara');
 
 
+create table teacher (
+    Teacher_ID char(10) constraint TEA_ID_PK primary key,
+    Name varchar(30),
+    Subject varchar(20),
+    Phone char(10),
+    Aadhar char(12),
+    Email varchar(30),
+    Address varchar(50)
+    );
+    insert into teacher values ('TCH0001', 'Akhandanand Tripathi', 'DBMS', '6225474585', '123524789652', 'teacher@gmail.com', 'Vadodara');
+
+
+
+
 -- Read 
 select *from student;
 
@@ -19,3 +33,7 @@ update student set address = 'Uttar Pradesh' where Roll_Number = '24PG11000';
 
 -- Delete
 delete from student where Roll_Number = '24PG11000';
+
+
+-- Searching by Keywords
+select * from student where Name like '%Guddu%';
