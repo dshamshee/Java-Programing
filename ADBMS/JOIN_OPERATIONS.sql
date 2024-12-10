5829f9afdf3b1df9c7f551e3cdee0e1805c714cf
Danish <danishshamshee123@gmail.com>
2024-12-10 23:50:37 +0530

    adding coments on join operations

--- ADBMS/JOIN_OPERATIONS.sql
+++ ADBMS/JOIN_OPERATIONS.sql
@@ -17,37 +17,37 @@ insert into Employees values ('Emp03', 'Employee 3', NULL);
 
 
 
--- Inner Join 
+-- Inner Join (Shows employees with a valid department.)
 SELECT Employees.EmpID, Employees.EmpName, Departments.DeptName
 FROM Employees
 INNER JOIN Departments ON Employees.DeptID = Departments.DeptID;
 
 
--- Left Join 
+-- Left Join (Shows all employees, including those without a department.)
 SELECT Employees.EmpID, Employees.EmpName, Departments.DeptName
 FROM Employees
 LEFT JOIN Departments ON Employees.DeptID = Departments.DeptID;
 
 
--- Right Join
+-- Right Join (Shows all departments, including those without employees.)
 SELECT Employees.EmpID, Employees.EmpName, Departments.DeptName
 FROM Employees
 RIGHT JOIN Departments ON Employees.DeptID = Departments.DeptID;
 
 
--- Full OUter Join 
+-- Full OUter Join (Combines LEFT JOIN and RIGHT JOIN.)
 SELECT Employees.EmpID, Employees.EmpName, Departments.DeptName
 FROM Employees
 FULL OUTER JOIN Departments ON Employees.DeptID = Departments.DeptID;
 
 
--- Cross Join 
+-- Cross Join (Matches every employee to every department.)
 SELECT Employees.EmpID, Employees.EmpName, Departments.DeptName
 FROM Employees
 CROSS JOIN Departments;
 
 
--- Self Join 
+-- Self Join (Compares rows within the same table.)
 SELECT E1.EmpName AS Employee, E2.EmpName AS Manager
 FROM Employees E1
 INNER JOIN Employees E2 ON E1.DeptID = E2.DeptID;
